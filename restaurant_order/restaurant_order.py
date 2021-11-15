f19e90ee9cf88cb7344283a6a80702382ac68b56
chlhugo01 <59417093+chlhugo01@users.noreply.github.com>
2021-11-15 20:45:45 +0800

    [feat] Add 3 Redis events

--- restaurant_order/restaurant_order.py
+++ restaurant_order/restaurant_order.py
@@ -16,6 +16,41 @@ redis_conn = redis.Redis(host='message_queue', port=6379)
 ################
 
 
+def new_order(message):
+    load = json.loads(message['data'])
+    order_id = load['order_id']
+    restaurant_id = load['restaurant_id']
+    food_id = load['food_id']
+    user_id = load['user_id']
+    orderresult = mongo_client.restaurant_orders.order.find_one({"restaurant_id": int(restaurant_id)})
+    query = {"_id" : orderresult["_id"] }
+    orderresult["order"].append({'order_id':order_id, 'customer_id': user_id,'food_id':food_id,'prepare':0,'deliver':0})
+    mongo_client.restaurant_orders.order.replace_one( query, orderresult )
+    #orderresult = mongo_client.restaurant_orders.Order.find()
+
+def set_prepared(message):
+    load = json.loads(message['data'])
+    order_id = load['order_id']
+    prepare = load['prepared']
+    orders = mongo_client.restaurant_orders.order.find_one({'order.order_id': order_id})
+    for order in orders["order"]:
+        if order['order_id'] == order_id:
+            order["prepare"] = 1
+            break
+    mongo_client.restaurant_orders.order.replace_one({'_id': orders['_id']}, orders)
+
+def set_shipped(message):
+    load = json.loads(message['data'])
+    order_id = load['order_id']
+    delivery_id = load['delivery_id']
+    orders = mongo_client.restaurant_orders.order.find_one({'order.order_id': order_id})
+    for order in orders["order"]:
+        if order['order_id'] == order_id:
+            order['deliver'] = delivery_id
+            break
+    mongo_client.restaurant_orders.order.replace_one({'_id': orders['_id']}, orders)
+
+    
 ###################
 # Flask endpoints
 ###################
